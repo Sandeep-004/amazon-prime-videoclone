6e32607bd610d5b77071dcba888e4a624a81caf9
Sandeep <sharmas175@gmail.com>
2021-04-20 23:48:49 +0530

    modified deleted files

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ const PORT = process.env.PORT || 3001;
 
 if(process.env.Node_ENV == "production") {
     app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 app.listen(3001, () => {
